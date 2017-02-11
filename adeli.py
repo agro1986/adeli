7d539d0b4bc8450bc26f4254e6648b8f7ffaa375
Agro Rachmatullah <agro1986@gmail.com>
2017-02-12 00:55:46 +0900

    Add many functions and unit testing

--- adeli.py
+++ adeli.py
@@ -38,3 +38,84 @@ def dot(x, y):
 
     return total
 
+
+def is_diagonal(m):
+    for i in range(matrix_row(m)):
+        for j in range(matrix_col(m)):
+            if i == j:
+                continue
+            if m[i][j] != 0:
+                return False
+    return True
+
+
+def matrix_diagonal(v):
+    size = len(v)
+    m = matrix(size, size)
+    for i in range(size):
+        m[i][i] = v[i]
+    return m
+
+
+def matrix_identity(n):
+    return matrix_diagonal([1.0] * n)
+
+
+def is_identity(m):
+    row = matrix_row(m)
+    col = matrix_col(m)
+    if row != col:
+        return False
+    for i in range(row):
+        for j in range(col):
+            if i == j:
+                if m[i][j] != 1.0:
+                    return False
+            elif m[i][j] != 0.0:
+                return False
+    return True
+
+
+def elementwise_product(m, n):
+    row = matrix_row(m)
+    col = matrix_col(m)
+    row2 = matrix_row(n)
+    col2 = matrix_col(n)
+    if row2 != row or col2 != col:
+        raise ValueError(f"The size of m ({row}x{col}) does not match n ({row2}x{col2}).")
+    o = matrix(row, col)
+    for i in range(row):
+        for j in range(col):
+            o[i][j] = m[i][j] * n[i][j]
+    return o
+
+
+def matrix_equal(m, n):
+    row = matrix_row(m)
+    col = matrix_col(m)
+    row2 = matrix_row(n)
+    col2 = matrix_col(n)
+    if row2 != row or col2 != col:
+        return False
+    for i in range(row):
+        for j in range(col):
+            if m[i][j] != n[i][j]:
+                return False
+    return True
+
+def matrix_transpose(m):
+    row = matrix_row(m)
+    col = matrix_col(m)
+    mt = matrix(col, row)
+    for i in range(row):
+        for j in range(col):
+            mt[j][i] = m[i][j]
+    return mt
+
+def matrix_is_square(m):
+    return matrix_row(m) == matrix_col(m)
+
+
+def matrix_is_symmetric(m):
+    mt = matrix_transpose(m)
+    return matrix_equal(m, mt)
--- test.py
+++ test.py
@@ -1,21 +1,68 @@
 from adeli import *
+import unittest
 
-print("Hello")
-a = [1, 2, 3]
-b = [4, 5, 6]
-c = [7, 8]
 
-#dot(1, 2)
-print(dot(a, b))
-#print(dot(a, c))
+class TestAdeli(unittest.TestCase):
+    def test_dot(self):
+        a = [1, 2]
+        b = [3, 4]
+        self.assertEqual(dot(a, b), 11)
 
-print("Done")
+    def test_matrix(self):
+        row = 3
+        col = 2
+        m = matrix(row, col)
+        self.assertEqual(matrix_row(m), row)
+        self.assertEqual(matrix_col(m), col)
+        for i in range(row):
+            for j in range(col):
+                self.assertEqual(m[i][j], 0.0)
 
-m = matrix(3, 2)
-m[0][0] = 3.0
-m[0][1] = 4.0
-print(m)
-print(matrix_row(m))
-print(matrix_col(m))
+    def test_frobenius_norm(self):
+        m = matrix(3, 2)
+        m[0][0] = 3.0
+        m[0][1] = 4.0
+        self.assertEqual(frobenius_norm(m), 5.0)
 
-print(frobenius_norm(m))
+    def test_identity_matrix(self):
+        m = matrix_identity(4)
+        for i in range(matrix_row(m)):
+            for j in range(matrix_col(m)):
+                if i == j:
+                    self.assertEqual(m[i][j], 1.0, f"row {i} col {j}")
+                else:
+                    self.assertEqual(m[i][j], 0.0, f"row {i} col {j}")
+        self.assertTrue(is_identity(m))
+
+    def test_diagonal_matrix(self):
+        m = matrix_diagonal([1, 2, 3])
+        self.assertTrue(is_diagonal(m))
+        self.assertEqual(matrix_row(m), 3)
+        self.assertEqual(m[0][0], 1)
+        self.assertEqual(m[1][1], 2)
+        self.assertEqual(m[2][2], 3)
+
+    def test_matrix_equality(self):
+        m = matrix(2, 2)
+        n = matrix(2, 3)
+        self.assertTrue(matrix_equal(m, m))
+        self.assertFalse(matrix_equal(m, n))
+
+    def test_elementwise_product(self):
+        m = [[1, 2],  [3,   4]]
+        n = [[5, 6],  [7,   8]]
+        o1 = [[5, 12], [21, 32]]
+        o2 = elementwise_product(m, n)
+        self.assertTrue(matrix_equal(o1, o2))
+
+    def test_matrix_symmetric(self):
+        m = [[1, 3],
+             [3, 1]]
+        self.assertTrue(matrix_is_symmetric(m))
+
+        n = [[1, 3],
+             [2, 1]]
+        self.assertFalse(matrix_is_symmetric(n))
+
+if __name__ == '__main__':
+    unittest.main()
